9350ff8efc0a07fc1506db317d2b5a7e6623ff67
Max Hassen <max27183141@gmail.com>
2024-03-28 16:46:04 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '9000',
+    static: ['./public'],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
